91f5ce3efd69134d3261609198ef575cfe51baad
gcp1351581365 <58849326+gcp1351581365@users.noreply.github.com>
2020-05-14 10:28:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,8 +4,8 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Author: z
+# Blog: 
 #============================================================
 
 # Modify default IP
